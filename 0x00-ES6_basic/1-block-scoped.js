be2a8adfda35d1a02300aad01f15e09dfc894465
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-03-08 17:19:45 +0200

    es lint fixes

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-    if (trueOrFalse) {
-      //
-    }
-  
-    const task = false;
-    const task2 = true;
-  
-    return [task, task2];
+  if (trueOrFalse) {
+    //
   }
+  const task = false;
+  const task2 = true;
+  return [task, task2];
+}
