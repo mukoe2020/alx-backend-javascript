4a69295756125b5b443a404edb29a0a11f9624b5
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-03-08 18:56:06 +0200

    es lint fixes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      resolve('Sucess');
-    });
-  }
+  return new Promise((resolve) => {
+    resolve('Sucess');
+  });
+}
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,12 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      const value = mathFunction();
-      queue.push(value);
-    } catch (e) {
-      queue.push(e.toString());
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-    return queue;
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch (e) {
+    queue.push(e.toString());
+  } finally {
+    queue.push('Guardrail was processed');
   }
+  return queue;
+}
