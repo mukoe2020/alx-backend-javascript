af034da6e7af0e5193dbb610158bf73c58a0f3e0
memoemukoe <122824378+mukoe2020@users.noreply.github.com>
2024-03-12 08:35:43 +0200

    using filter function

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,3 @@
-export default function getStudentIdsSum(arr) {
-  const combo = arr.reduce((total, student) => total + student.id, 0);
-  return combo;
+export default function getStudentsByLocation(arr, city) {
+  return arr.filter((currentElement) => currentElement.location === city);
 }
